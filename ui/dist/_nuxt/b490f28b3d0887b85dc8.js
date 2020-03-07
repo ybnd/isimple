75f3c71f801ce4c2b8d8b6d587452cbaeedf6f13
ybnd <ybnd@tuta.io>
2020-03-07 09:48:11 +0100

    Working server/webapp ~ .app.py & ui/dist
    * hosted by Waitress
    * ui/dist is served over Flask
    * server stops when app tab/window is closed

---
+++ ui/dist/_nuxt/b490f28b3d0887b85dc8.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{177:function(t,e,n){"use strict";n.r(e);var r={name:"Try"},c=n(22),component=Object(c.a)(r,(function(){var t=this.$createElement;this._self._c;return this._m(0)}),[function(){var t=this.$createElement,e=this._self._c||t;return e("div",[e("p",[this._v("wtf")])])}],!1,null,"484dd6c2",null);e.default=component.exports}}]);
